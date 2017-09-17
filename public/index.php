a32e2d99cea743168fb580900e06c73f63fb4def
Simon Jones <simonj@redbullet.co.uk>
2017-09-17 23:58:53 +0100

    add index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$container = new League\Container\Container;
+
+$container->share('response', Zend\Diactoros\Response::class);
+$container->share('request', function () {
+    return Zend\Diactoros\ServerRequestFactory::fromGlobals(
+        $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+    );
+});
+
+$container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
+
+$router = new League\Route\RouteCollection($container);
+
+require (__DIR__ . '/../app/routes.php');
+
+$response = $router->dispatch($container->get('request'), $container->get('response'));
+
+$container->get('emitter')->emit($response);
